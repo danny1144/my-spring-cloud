6c47e27c61eeec5c60c1bd3f9f2f9d06b5efcd45
z00403vj <xiaoping.zhong.ext@siemens.com>
2019-09-23 17:53:06 +0800

    :art:fix bug

--- ldapModle/src/main/java/com/tz/ldap/controller/LdapController.java
+++ ldapModle/src/main/java/com/tz/ldap/controller/LdapController.java
@@ -12,12 +12,22 @@ public class LdapController {
     @Resource
     private LdapService ldapService;
 
+    /**
+     * 添加用户
+     * @return
+     * @throws Exception
+     */
     @RequestMapping("addUser")
     public  Object addUser() throws Exception {
         ldapService.addUser();
         return "success";
     }
 
+    /**
+     * 添加用户组并添加用户至用户组
+     * @return
+     * @throws Exception
+     */
     @RequestMapping("addUserGroup")
     public  Object addUserGroup() throws Exception {
         ldapService.addUserGroup();
