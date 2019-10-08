7346d47b275c56fb7e46e5a27f125c2c5751fce3
z00403vj <xiaoping.zhong.ext@siemens.com>
2019-10-08 17:02:53 +0800

    :art:fix bug

--- ldapModle/src/main/java/com/tz/ldap/config/ExceptionResove.java
+++ ldapModle/src/main/java/com/tz/ldap/config/ExceptionResove.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
-import org.springframework.web.multipart.MultipartException;
 
 import javax.servlet.http.HttpServletRequest;
 
