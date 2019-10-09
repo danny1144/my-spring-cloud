7ac512940794f57f436b3c93b4d038d72c67a974
z00403vj <xiaoping.zhong.ext@siemens.com>
2019-10-09 12:38:54 +0800

    :art:fix bug

--- ldapModle/src/main/java/com/siemens/ldap/config/SptingDataLdapConfig.java
+++ ldapModle/src/main/java/com/siemens/ldap/config/SptingDataLdapConfig.java
@@ -2,6 +2,7 @@ package com.siemens.ldap.config;
 
 import com.icitic.ldap.UserDAO;
 import com.siemens.ldap.util.LdapUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
@@ -13,16 +14,27 @@ import org.springframework.ldap.core.support.LdapContextSource;
 @EnableLdapRepositories
 public class SptingDataLdapConfig {
 
+	@Value("${ldap.base}")
+	private String base;
+	@Value("${ldap.url}")
+	private String url;
+	@Value("${ldap.userDn}")
+	private String userDn;
+	@Value("${ldap.password}")
+	private String password;
+	@Value("${ldap.referral}")
+	private String referral;
+
 	@Bean
 	ContextSource contextSource() {
  
 		LdapContextSource ldapContextSource = new LdapContextSource();
-		ldapContextSource.setBase("ou=users,dc=siemens,dc=com");
-		ldapContextSource.setUrl("ldap://localhost:389");
+		ldapContextSource.setBase(base);
+		ldapContextSource.setUrl(url);
 		//rootDn用户
-		ldapContextSource.setUserDn("cn=Manager,dc=siemens,dc=com");
-		ldapContextSource.setPassword("123456");
-		ldapContextSource.setReferral("follow");
+		ldapContextSource.setUserDn(userDn);
+		ldapContextSource.setPassword(password);
+		ldapContextSource.setReferral(referral);
 		return ldapContextSource;
 	}
  
